1f99c797be7fa6eb547d1458081f4c4eb4b8e8be
Martín <maguera37@gmail.com>
2023-01-25 19:15:45 -0300

    feat: I deleted files .js and .css

--- src/js/classes/todo.js
+++
@@ -1,8 +0,0 @@
-export class Todo {
-  constructor(task) {
-    this.created = new Date()
-    this.id = new Date().getTime().toString()
-    this.task = task
-    this.isCompleted = false
-  }
-}
--- src/js/helpers/removeAllTodo.js
+++
@@ -1,7 +0,0 @@
-import { ContainerList } from "../components"
-
-export const REMOVE_ALL_TODO = todoList => {
-  todoList.removeAllTodo()
-
-  ContainerList.remove()
-}
